66915030c46fd5d28a7d30bb1371629417bd2af8
Elco Jacobs <elco@brewpi.com>
2017-07-03 21:28:59 +0200

    added vuex

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ require(`quasar/dist/quasar.${__THEME}.css`);
 import Vue from 'vue';
 import Quasar from 'quasar';
 import router from './router';
+import store from './store'; 
 
 Vue.config.productionTip = false;
 Vue.use(Quasar); // Install Quasar Framework
@@ -32,6 +33,7 @@ Quasar.start(() => {
   new Vue({
     el: '#q-app',
     router,
+    store,
     render: h => h(require('./App')),
   });
 });
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    test: 'test',
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+  strict: process.env.NODE_ENV !== 'production',
+});
