fca9825e2f8fc5b1da80ac125c3385453e0b0105
Elco Jacobs <elco@brewpi.com>
2017-08-25 21:16:23 +0200

    WIP

---
+++ src/components/GridTile.vue
@@ -0,0 +1,30 @@
+<template>
+<div>  
+</div>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'GridTile',
+  props: {
+  },
+  data: () => ({
+  }),
+  created() {
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted() {
+  },
+  beforeDestroy() {
+  },
+};
+
+</script>
+
+<style lang="stylus">
+</style>
