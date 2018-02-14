b96d50931e42757a56f366d115f18b62ed8e9940
Gaya Kessler <gaya.kessler@gmail.com>
2018-02-14 15:57:36 +0100

    init quasar project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  extends: ['plugin:vue/strongly-recommended', 'airbnb-base'],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true
+  },
+  // add your custom rules here
+  'rules': {
+    'no-param-reassign': 0,
+
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# BrewBlox UI
+
+> UI representation of the BrewBlox project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:8080
+$ quasar dev
+```
