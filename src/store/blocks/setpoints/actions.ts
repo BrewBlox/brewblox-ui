ad3c37834b0ecef46202247442aaead65af0cbb5
Gaya Kessler <gaya.kessler@gmail.com>
2018-02-21 11:02:14 +0100

    add setting to setpoint

--- src/store/blocks/setpoints/actions.ts
+++ src/store/blocks/setpoints/actions.ts
@@ -3,6 +3,6 @@ import store from '../../';
 import { addBlock } from '../mutations';
 import { SetPoint } from './setpoint';
 
-export const addSetPoint = ({ id, value }: SetPoint) => {
-  addBlock(store, { id, value, type: 'setpoint' });
+export const addSetPoint = ({ id, value, setting }: SetPoint) => {
+  addBlock(store, { id, value, setting, type: 'setpoint' });
 };
--- src/store/blocks/setpoints/setpoint.d.ts
+++ src/store/blocks/setpoints/setpoint.d.ts
@@ -2,6 +2,7 @@ import { BlockBase } from '../state';
 
 export interface SetPoint extends BlockBase {
   value: number;
+  setting: number;
 }
 
 export interface SetPointBlock extends SetPoint {
