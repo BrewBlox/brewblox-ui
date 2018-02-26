c615fb27867501e4108274177c7130503bd486a0
Gaya Kessler <gaya.kessler@gmail.com>
2018-02-26 17:29:19 +0100

    add comments for block types imported

--- src/components/blocks/block.vue
+++ src/components/blocks/block.vue
@@ -11,9 +11,12 @@ import Vue from 'vue';
 import { blockById } from '../../store/blocks/getters';
 
 const blockTypes = {
+  // Block type components
   OneWireTempSensor: () => import('./OneWireTempSensor/default.vue'),
   SetPointSimple: () => import('./SetPointSimple/default.vue'),
   SensorSetPointPair: () => import('./SensorSetPointPair/default.vue'),
+
+  // fallback component when type is not resolved
   Unknown: () => import('./Unknown.vue'),
 };
 
