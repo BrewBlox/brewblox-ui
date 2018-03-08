15ea03128e2ac654b773b3a9bebe2e18adfdcb4e
Gaya Kessler <gaya.kessler@gmail.com>
2018-03-08 15:04:54 +0100

    ability to update SetPoint

--- src/components/blocks/SetPointSimple/SetPointSimple.ts
+++ src/components/blocks/SetPointSimple/SetPointSimple.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 import { getById } from '../../../store/blocks/SetPointSimple/getters';
+import { persist } from '../../../store/blocks/SetPointSimple/actions';
 
 @Component({
   props: {
@@ -12,11 +13,34 @@ import { getById } from '../../../store/blocks/SetPointSimple/getters';
   },
 })
 export default class SetPointSimple extends Vue {
+  valueInput = 0;
+
   get blockData() {
     return getById(this.$props.id);
   }
 
   get settings() {
     return this.blockData.settings;
+  }
+
+  get loading() {
+    return !!this.blockData.isLoading;
+  }
+
+  get changed() {
+    return this.settings.value !== this.valueInput;
+  }
+
+  mounted() {
+    this.valueInput = this.settings.value;
+  }
+
+  update() {
+    persist({
+      id: this.$props.id,
+      settings: {
+        value: this.valueInput,
+      },
+    });
   }
 }
--- src/components/blocks/SetPointSimple/default.vue
+++ src/components/blocks/SetPointSimple/default.vue
@@ -7,12 +7,26 @@
         <q-list-header>Settings</q-list-header>
         <q-item>
           <q-item-main>
-            <q-item-tile label>Value</q-item-tile>
-            <q-item-tile sublabel>{{ settings.value }}</q-item-tile>
+            <q-input
+              v-model="valueInput"
+              stack-label="Value"
+              placeholder="Value of SetPoint"
+              type="number"
+            />
           </q-item-main>
         </q-item>
       </q-list>
     </q-card-main>
+
+    <q-btn
+      :loading="loading"
+      icon="check"
+      :color="changed ? 'primary' : 'light'"
+      :disable="!changed"
+      @click="update"
+    >
+      Save
+    </q-btn>
   </q-card>
 </template>
 
