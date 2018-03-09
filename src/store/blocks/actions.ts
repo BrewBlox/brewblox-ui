2d827b9c89043810a8e2f2db8f61e0a983c88acb
Gaya Kessler <gaya.kessler@gmail.com>
2018-03-09 07:49:52 +0100

    put addBlock in seperate file

--- src/store/blocks/actions.ts
+++ src/store/blocks/actions.ts
@@ -3,40 +3,24 @@ import { getStoreAccessors } from 'vuex-typescript';
 import { fetchBlock, fetchBlocks, persistBlock as persistBlockToApi } from './api';
 
 import store from '../';
-import { BlocksState, BlocksContext, Block, BlockSaveBase } from './state';
+import { BlocksState, BlocksContext, BlockSaveBase } from './state';
 import { State as RootState } from '../state';
+import addBlock from './add-block';
 
 import {
   mutateBlock as mutateBlockInStore,
   mutateFetching as mutateFetchingInStore,
 } from './mutations';
 
-import { addSetPoint } from './SetPointSimple/actions';
-import { addOneWireTempSensor } from './OneWireTempSensor/actions';
-import { addSensorSetPointPair } from './SensorSetPointPair/actions';
-
 const { dispatch } = getStoreAccessors<BlocksState, RootState>('blocks');
-
-function addBlock(block: Block) {
-  switch (block.type) {
-    case 'SensorSetPointPair':
-      addSensorSetPointPair(block);
-      break;
-    case 'OneWireTempSensor':
-      addOneWireTempSensor(block);
-      break;
-    case 'SetPointSimple':
-      addSetPoint(block);
-      break;
-    default:
-      throw new Error('Invalid block type');
-  }
-}
 
 const actions = {
   async findBlock(context: BlocksContext, id: string) {
     // will fetch a block from the server
-    addBlock(await fetchBlock(id));
+    const block = await fetchBlock(id);
+
+    // add block to store
+    addBlock(block);
   },
   async listBlocks() {
     // update isFetching
