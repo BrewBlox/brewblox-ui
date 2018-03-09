a10370a14c49542a3f0d5b94be8b598c91b8afbc
Gaya Kessler <gaya.kessler@gmail.com>
2018-03-09 11:13:52 +0100

    add metrics page

---
+++ src/components/metrics/metrics.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Dit is een metrics test</div>
+</template>
+
+<script>
+export default {
+  name: 'Metrics',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/metrics.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page padding>
+    <Metrics id="controller-1/sensor-1" />
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import Metrics from '../components/metrics/metrics.vue';
+
+export default Vue.extend({
+  name: 'PageIndex',
+  components: { Metrics },
+  methods: {},
+});
+</script>
