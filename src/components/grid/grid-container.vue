c7ee01b4e73614813a1113a14567f23e44b0f279
Gaya Kessler <gaya.kessler@gmail.com>
2018-03-21 11:25:53 +0100

    grid styling

--- src/components/grid/grid-container.vue
+++ src/components/grid/grid-container.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="grid-container">
-    <slot />
+    <div class="grid-main-container">
+      <slot />
+    </div>
+    <div
+      v-if="false"
+      class="grid-container-overlay"
+    >
+      <div class="grid-container-overlay-grid" />
+    </div>
   </div>
 </template>
 
@@ -13,10 +21,37 @@ export default {
 <style scoped>
 .grid-container {
   background-color: transparent;
+  position: relative;
+}
+
+.grid-main-container, .grid-container-overlay {
   display: grid;
   grid-gap: 20px;
   grid-template-columns: repeat(auto-fill, 100px);
   grid-auto-rows: 100px;
   justify-content: center;
+}
+
+.grid-container-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  grid-template-rows: 1fr;
+}
+
+.grid-container-overlay-grid {
+  background-size: 120px 120px;
+  background-image:
+    linear-gradient(#121a1f 20px, transparent 0px),
+    linear-gradient(90deg, #121a1f 20px, transparent 0px),
+    linear-gradient(#fff, #fff);
+  background-position: 0 -20px, -20px, 0 0;
+  grid-column-start: 1;
+  grid-column-end: -1;
+  grid-row-start: 1;
+  grid-row-end: -1;
+  opacity: 0.03;
 }
 </style>
