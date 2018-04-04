eed95bb137a939a10c7ab0d2621cfedc89084af5
Gaya Kessler <gaya.kessler@gmail.com>
2018-04-04 10:26:29 +0200

    move mutations to helpers file to avoid store conflict in jest

---
+++ src/store/dashboards/mutations.helpers.ts
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import { merge } from 'lodash';
+
+import { Dashboard, DashboardItem, DashboardState } from './state';
+
+function updateDashboardItem(state: DashboardState, id: string, newData: any) {
+  Vue.set(state.items, 'byId', Object.assign(
+    {},
+    state.items.byId,
+    { [id]: merge(state.items.byId[id], newData) },
+  ));
+}
+
+const mutations = {
+  addDashboard(state: DashboardState, dashboard: Dashboard) {
+    state.dashboards.allIds.push(dashboard.id);
+    state.dashboards.byId[dashboard.id] = { ...dashboard };
+  },
+  addDashboardItem(state: DashboardState, item: DashboardItem) {
+    state.items.allIds.push(item.id);
+    state.items.byId[item.id] = { ...item };
+  },
+  mutateFetching(state: DashboardState, fetching: boolean) {
+    state.fetching = fetching;
+  },
+  setDashboardItemOrder(state: DashboardState, { id, order }: { id: string, order: number }) {
+    updateDashboardItem(state, id, { order });
+  },
+  setDashboardItemSize(
+    state: DashboardState,
+    { id, cols, rows }: { id: string, cols: number, rows: number },
+  ) {
+    updateDashboardItem(state, id, { cols, rows });
+  },
+};
+
+export default mutations;
--- src/store/dashboards/mutations.ts
+++ src/store/dashboards/mutations.ts
@@ -1,43 +1,11 @@
-import Vue from 'vue';
 import { getStoreAccessors } from 'vuex-typescript';
-import { merge } from 'lodash';
 
+import mutations from './mutations.helpers';
 import { Dashboard, DashboardItem, DashboardState } from './state';
 import { State as RootState } from '../state';
 import store from '../index';
 
 const { commit } = getStoreAccessors<DashboardState, RootState>('dashboards');
-
-function updateDashboardItem(state: DashboardState, id: string, newData: any) {
-  Vue.set(state.items, 'byId', Object.assign(
-    {},
-    state.items.byId,
-    { [id]: merge(state.items.byId[id], newData) },
-  ));
-}
-
-const mutations = {
-  addDashboard(state: DashboardState, dashboard: Dashboard) {
-    state.dashboards.allIds.push(dashboard.id);
-    state.dashboards.byId[dashboard.id] = { ...dashboard };
-  },
-  addDashboardItem(state: DashboardState, item: DashboardItem) {
-    state.items.allIds.push(item.id);
-    state.items.byId[item.id] = { ...item };
-  },
-  mutateFetching(state: DashboardState, fetching: boolean) {
-    state.fetching = fetching;
-  },
-  setDashboardItemOrder(state: DashboardState, { id, order }: { id: string, order: number }) {
-    updateDashboardItem(state, id, { order });
-  },
-  setDashboardItemSize(
-    state: DashboardState,
-    { id, cols, rows }: { id: string, cols: number, rows: number },
-  ) {
-    updateDashboardItem(state, id, { cols, rows });
-  },
-};
 
 // exported commit accessors
 export const mutateFetching =
