af3fa03d0acbe8fb076124d1774aba1451ba9215
Gaya Kessler <gaya.kessler@gmail.com>
2018-04-04 14:55:00 +0200

    put App component in separate ts file

---
+++ src/App.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+import { fetchBlocks } from './store/blocks/actions';
+import { fetchDashboards } from './store/dashboards/actions';
+
+@Component
+class App extends Vue {
+  async created() {
+    // fetch all block and dashboard on init
+    await Promise.all([
+      fetchBlocks(),
+      fetchDashboards(this.$store),
+    ]);
+  }
+}
+
+export default App;
