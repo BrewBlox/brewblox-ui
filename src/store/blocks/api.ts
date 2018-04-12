d6eac8caf46ef6a599700fde5f6bc48cd69a2097
Gaya Kessler <gaya.kessler@gmail.com>
2018-04-12 14:08:57 +0200

    spread data object from api response

--- src/store/blocks/api.ts
+++ src/store/blocks/api.ts
@@ -2,12 +2,23 @@ import { Block, BlockSaveBase, MetricsResult, BlockBase } from './state';
 
 import { get, put, patch } from '@/core/fetch';
 
+function spreadData(input: any) {
+  const spreadInput = {
+    ...input,
+    ...input.data,
+  };
+
+  delete spreadInput.data;
+
+  return spreadInput;
+}
+
 export function fetchBlock(id: string): Promise<Block> {
-  return get(`/blocks/${encodeURIComponent(id)}`);
+  return get(`/blocks/${encodeURIComponent(id)}`).then(block => spreadData(block));
 }
 
 export function fetchBlocks(): Promise<Block[]> {
-  return get('/blocks/list');
+  return get('/blocks/list').then(blocks => blocks.map(spreadData));
 }
 
 export function persistBlock(block: BlockSaveBase): Promise<BlockSaveBase> {
