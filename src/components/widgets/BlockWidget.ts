8015bb97b3def1438538150a254e815e57148788
Gaya Kessler <gaya.kessler@gmail.com>
2018-04-12 10:59:56 +0200

    document block update implementation

--- src/components/widgets/BlockWidget.ts
+++ src/components/widgets/BlockWidget.ts
@@ -9,6 +9,24 @@ import Widget from './Widget';
 
 @Component
 class BlockWidget extends Widget {
+  /**
+   * Use inputMapping to bind sources of values to the inputs used in the component.
+   *
+   * Object looks like:
+   * {
+   *    keyInput: {
+   *      path: 'propertyName.propertyName.propertyName',
+   *      default: 'anything'
+   *    }
+   * }
+   *
+   * The purpose is to have a single source of truth, while not updating the source when updating
+   * the contents of the inputs.
+   *
+   * When the source is adjusted, the input value will be updated accordingly.
+   *
+   * 'path' in keyInput is used as 'path' is used in https://lodash.com/docs/4.17.5#get
+   */
   inputMapping: {
     [inputPropName: string]: {
       path: string,   // path to value in object
@@ -18,7 +36,11 @@ class BlockWidget extends Widget {
 
   inputs: { [inputPropName: string]: any } = {};
 
-  inputsFromState(): { [key: string]: any } {
+  /**
+   * Maps all the values found in the source to their respective input properties
+   * @returns {{[key: string]: any}}
+   */
+  inputsFromSource(): { [key: string]: any } {
     return Object.keys(this.inputMapping)
       .reduce(
         (total, key) => {
@@ -31,19 +53,30 @@ class BlockWidget extends Widget {
       );
   }
 
+  /**
+   * Checks for changes in the inputs compared to the source
+   * @returns {boolean}
+   */
   get changed() {
-    const state = this.inputsFromState();
+    const state = this.inputsFromSource();
 
     return Object.keys(state).some(key => state[key] !== this.inputs[key]);
   }
 
+  /**
+   * Block data from the store
+   * @returns {Block}
+   */
   get block(): Block {
     return blockById(this.$store, this.options.block);
   }
 
+  /**
+   * Updates the inputs when the source changes
+   */
   @Watch('block', { immediate: true, deep: true })
   onBlockUpdate() {
-    this.inputs = this.inputsFromState();
+    this.inputs = this.inputsFromSource();
   }
 }
 
