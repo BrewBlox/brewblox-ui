fd8b65d550604a3a336c4608d8c4395ecc4dc196
Gaya Kessler <gaya.kessler@gmail.com>
2018-04-18 08:28:07 +0200

    template for fetching settings

--- src/store/settings/actions.ts
+++ src/store/settings/actions.ts
@@ -1,1 +1,25 @@
-export default {};
+import { getStoreAccessors } from 'vuex-typescript';
+
+import { SettingsContext, SettingsState } from './state';
+import { State as RootState } from '../state';
+
+import { mutateFetching as mutateFetchingInStore } from './mutations';
+
+const { dispatch } = getStoreAccessors<SettingsState, RootState>('settings');
+
+const actions = {
+  async fetchSettings(context: SettingsContext) {
+    // update isFetching
+    mutateFetchingInStore(context, true);
+
+    // fetch and set settings
+
+    // update isFetching
+    mutateFetchingInStore(context, false);
+  },
+};
+
+// exported action accessors
+export const fetchSettings = dispatch(actions.fetchSettings);
+
+export default actions;
--- src/store/settings/mutations.ts
+++ src/store/settings/mutations.ts
@@ -1,1 +1,18 @@
-export default {};
+import { getStoreAccessors } from 'vuex-typescript';
+
+import { SettingsState } from './state';
+
+import { State as RootState } from '../state';
+
+const { commit } = getStoreAccessors<SettingsState, RootState>('settings');
+
+const mutations = {
+  mutateFetching(state: SettingsState, fetching: boolean) {
+    state.fetching = fetching;
+  },
+};
+
+// exported commit accessors
+export const mutateFetching = commit(mutations.mutateFetching);
+
+export default mutations;
