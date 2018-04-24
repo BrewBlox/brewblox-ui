4ce8cf6545b8e45470f2462fd3f2e8ba251dfd8c
Gaya Kessler <gaya.kessler@gmail.com>
2018-04-24 15:59:40 +0200

    refactor blocks/ to objects/

--- src/store/blocks/api.ts
+++ src/store/blocks/api.ts
@@ -4,21 +4,21 @@ import { spreadData, unspreadData } from '@/core/api-spread';
 import { Block, BlockSaveBase, MetricsResult, BlockBase } from './state';
 
 export function fetchBlocks(): Promise<Block[]> {
-  return get('/blocks').then(blocks => blocks.map(spreadData));
+  return get('/objects').then(blocks => blocks.map(spreadData));
 }
 
 export function fetchBlock(id: string): Promise<Block> {
-  return get(`/blocks/${encodeURIComponent(id)}`).then(block => spreadData(block));
+  return get(`/objects/${encodeURIComponent(id)}`).then(block => spreadData(block));
 }
 
 export function fetchBlockMetrics(id: string): Promise<MetricsResult> {
-  return get(`/blocks/${encodeURIComponent(id)}/metrics`);
+  return get(`/objects/${encodeURIComponent(id)}/metrics`);
 }
 
 export function persistBlock(block: BlockSaveBase): Promise<BlockSaveBase> {
-  return put(`/blocks/${encodeURIComponent(block.id)}`, unspreadData(block));
+  return put(`/objects/${encodeURIComponent(block.id)}`, unspreadData(block));
 }
 
 export function updateBlock(block: BlockBase & any): Promise<BlockSaveBase> {
-  return patch(`/blocks/${encodeURIComponent(block.id)}`, unspreadData(block));
+  return patch(`/objects/${encodeURIComponent(block.id)}`, unspreadData(block));
 }
