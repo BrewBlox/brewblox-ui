c384e869fccabee436ddeb5c290d924fa5e9a1bd
Gaya Kessler <gaya.kessler@gmail.com>
2018-04-25 12:54:32 +0200

    handle put and patch on correct objects

--- mocks/api/__controller_id__/objects/__block_id__/helpers.js
+++ mocks/api/__controller_id__/objects/__block_id__/helpers.js
@@ -1,9 +1,10 @@
 const list = require('../GET.json');
+const list2 = require('../GET_2.json');
 const metrics = require('../metrics.json');
 
 module.exports = {
-  getBlock(id) {
-    return list.find(item => item.id === id);
+  getBlock(id, controllerId) {
+    return (controllerId === 'controller-1' ? list : list2).find(item => item.id === id);
   },
   getBlockMetrics(id) {
     return metrics.find(item => item.id === id);
