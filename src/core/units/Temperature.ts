fb5e0ac4859dc56af64891f7b34634031f8050bc
Gaya Kessler <gaya.kessler@gmail.com>
2018-05-09 16:34:27 +0200

    have outputs of temperature units as nice formatted

--- src/core/units/Temperature.ts
+++ src/core/units/Temperature.ts
@@ -4,8 +4,16 @@ export class Temperature extends Unit {}
 
 export class Celsius extends Temperature {
   unit: string = 'celsius';
+
+  toString(): string {
+    return `${this.value} °C`;
+  }
 }
 
 export class Fahrenheit extends Temperature {
   unit: string = 'fahrenheit';
+
+  toString(): string {
+    return `${this.value} °F`;
+  }
 }
