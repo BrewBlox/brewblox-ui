28dc99ac245ca0e62dcaeea3a7303de00ad5740e
Gaya Kessler <gaya.kessler@gmail.com>
2018-05-16 14:54:31 +0200

    convert time to js readable time

--- src/components/widgets/Metrics/Metrics.vue
+++ src/components/widgets/Metrics/Metrics.vue
@@ -5,6 +5,10 @@ import Metrics from '@/components/metrics/Metrics.vue';
 
 import Widget from '../Widget';
 
+function toMicroSeconds(nanoseconds: number): number {
+  return Math.floor(nanoseconds / 1000000);
+}
+
 /* eslint-disable */
 @Component({
   components: {
@@ -16,13 +20,14 @@ class MetricsWidget extends Widget {
   plotly = {
     data: [
       {
-        type: 'bar',
-        x: [1, 2, 3],
+        type: 'scatter',
+        x: [1526471406456307000, 1526471411462286000, 1526471416486398000].map(toMicroSeconds),
         y: [2, 5, 3],
       },
     ],
     layout: {
       title: 'A Fancy Plot',
+      xaxis: { type: 'date' },
     },
   };
 }
