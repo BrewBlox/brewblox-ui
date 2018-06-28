8df1d7adf18aabb6e2cea0f4388453b7a49e5814
Gaya Kessler <gaya.kessler@gmail.com>
2018-06-28 15:36:14 +0200

    use serialize and deserialize in fetch

--- src/core/fetch.ts
+++ src/core/fetch.ts
@@ -1,3 +1,5 @@
+import { serialize, deserialize } from '@/core/units/parseObject';
+
 const host = '/api';
 
 function toJson(result: Promise<Response>): Promise<any> {
@@ -9,7 +11,11 @@ function toJson(result: Promise<Response>): Promise<any> {
 
       return response;
     })
-    .then(response => response.json());
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      deserialize(data);
+    });
 }
 
 export function get(url: string): Promise<any> {
@@ -21,7 +27,7 @@ export function post(url: string, data: any, method = 'POST'): Promise<any> {
     `${host}${url}`,
     {
       method,
-      body: JSON.stringify(data),
+      body: JSON.stringify(serialize(data)),
       headers: new Headers({
         'Content-Type': 'application/json',
       }),
