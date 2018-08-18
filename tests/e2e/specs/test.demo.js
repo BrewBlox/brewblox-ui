3e7268102de4ad21c029caf71da113bd9664ca0a
Bob Steers <kargathia@hotmail.com>
2018-08-18 14:24:24 +0200

    proof of concept cypress

--- tests/e2e/specs/test.demo.js
+++ tests/e2e/specs/test.demo.js
@@ -3,6 +3,17 @@
 describe('My First Test', () => {
   it('Visits the Kitchen Sink', () => {
     cy.visit('/');
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
+    // click hamburger menu
+    cy.get('.q-toolbar > .q-btn > .q-btn-inner > .q-icon').click();
+    cy.contains('Blocks').click();
+    cy.contains('spark/sensor-setpoint-pair-1');
+    cy.contains('Home Dashboard').click();
+    // click only button in toolbar (edit dashboard)
+    cy.get('.toolbar-buttons > .q-btn').click();
+    // add widget
+    cy.get('.toolbar-buttons > .q-btn').first().click();
+    cy.contains('Sensor SetPoint Pair').click();
+    cy.get('.q-stepper-nav > .q-btn').click();
+    cy.contains('Choose a block').click();
   });
 });
