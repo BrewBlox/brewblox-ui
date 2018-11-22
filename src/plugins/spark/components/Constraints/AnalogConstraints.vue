3daeb95b23a19fd81e85e8dd50082c222398df38
Elco Jacobs <elco@brewpi.com>
2018-11-22 18:35:51 +0100

    Show limits for analog constraints

--- src/plugins/spark/components/Constraints/AnalogConstraints.vue
+++ src/plugins/spark/components/Constraints/AnalogConstraints.vue
@@ -3,15 +3,29 @@ import { BalancerLink } from '@/helpers/units/KnownLinks';
 import Component from 'vue-class-component';
 import Constraints, { ConstraintInfo } from './Constraints';
 
-@Component
+@Component({
+  props: {
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
 export default class AnalogConstraints extends Constraints {
   get constraintOptions() {
-    return [
-      'min',
-      'max',
-      'balancer',
-    ]
-      .map(v => ({ label: v, value: v }));
+    return [...this.labels()].map(([k, v]) => ({ label: v, value: k }));
+  }
+
+  labels() {
+    return new Map([
+      ['min', 'Minimum'],
+      ['max', 'Maximum'],
+      ['balanced', 'Balanced'],
+    ]);
+  }
+
+  label(k : string) {
+    return this.labels().get(k);
   }
 
   fieldType(key: string) {
@@ -19,7 +33,7 @@ export default class AnalogConstraints extends Constraints {
       case 'min':
       case 'max':
         return 'InputPopupEdit';
-      case 'balancer':
+      case 'balanced':
         return 'LinkPopupEdit';
       default:
         return null;
@@ -28,8 +42,8 @@ export default class AnalogConstraints extends Constraints {
 
   createConstraint(key: string, value: any = null): ConstraintInfo {
     switch (key) {
-      case 'balancer':
-        return { key, value: new BalancerLink(value) };
+      case 'balanced':
+        return { key, value: { balancerId: new BalancerLink(value) } };
       default:
         return { key, value: 0 };
     }
@@ -40,13 +54,19 @@ export default class AnalogConstraints extends Constraints {
 <template>
   <div class="column gutter-y-xs">
 
-    <div v-for="(cinfo, idx) in constraints" :key="idx" class="row gutter-x-xs">
-      <SelectPopupEdit clearable class="col-4" label="Constraint type" :options="constraintOptions" :field="cinfo.key" :change="callAndSaveConstraints(k => constraints[idx] = createConstraint(k))" />
-      <component :is="fieldType(cinfo.key)" class="col" label="Constraint value" type="number" :serviceId="serviceId" :field="cinfo.value" :change="callAndSaveConstraints(v => cinfo.value = v)" />
-      <q-btn class="col-1" icon="delete" @click="removeConstraint(idx); saveConstraints();" />
+    <div v-for="(cinfo, idx) in constraints" :key="idx">
+      <div :class="{row: true, limiting: cinfo.limiting}" v-if="readonly">
+        <span class="col">{{ label(cinfo.key) }}</span>
+        <span class="col">{{ (cinfo.value.granted || cinfo.value) | unit }}</span>
+      </div>
+      <div class="row" v-else>
+        <SelectPopupEdit clearable class="col-4" label="Constraint type" :options="constraintOptions" :field="cinfo.key" :change="callAndSaveConstraints(k => constraints[idx] = createConstraint(k))" />
+        <component :is="fieldType(cinfo.key)" class="col" label="Constraint value" type="number" :serviceId="serviceId" :field="cinfo.value.balancerId || cinfo.value" :change="callAndSaveConstraints(v => cinfo.value = v)" />
+        <q-btn class="col-1" icon="delete" @click="removeConstraint(idx); saveConstraints();" />
+      </div>
     </div>
 
-    <div class="row gutter-x-cs">
+    <div v-if="!readonly" class="row gutter-x-cs">
       <q-btn label="Add constraint">
         <q-popover>
           <q-list separator link>
@@ -62,4 +82,7 @@ export default class AnalogConstraints extends Constraints {
 </template>
 
 <style scoped>
+.limiting {
+  color: red;
+}
 </style>
