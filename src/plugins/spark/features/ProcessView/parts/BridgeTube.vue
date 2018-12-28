056a1428ef7e23aab0ea24fd08c688749726d8d4
Bob Steers <steers.bob@gmail.com>
2018-12-28 14:11:42 +0100

    implement animated arrows

--- src/plugins/spark/features/ProcessView/parts/BridgeTube.vue
+++ src/plugins/spark/features/ProcessView/parts/BridgeTube.vue
@@ -21,70 +21,12 @@ export default class BridgeTube extends PartComponent {
     };
   }
 
-  get lowDirection() {
-    return this.flowOnAngle(UP) > 0 ? DOWN : UP;
+  get lowReversed() {
+    return this.flowOnAngle(DOWN) > 0;
   }
 
-  get highDirection() {
-    return this.flowOnAngle(LEFT) > 0 ? RIGHT : LEFT;
-  }
-
-  get lowArrow() {
-    const y = this.frame * SQUARE_SIZE;
-    return {
-      x: 21,
-      y: this.lowDirection === DOWN ? (75 - y) - 6 : y,
-    };
-  }
-
-  highArrows(frame: number) {
-    // const points = {
-    //   start: { x: 50, y: 21 },
-    //   end: { x: 0, y: 21 },
-    //   ctrl: { x: 25, y: 1 },
-    // };
-
-    // if (this.highDirection === LEFT) {
-    //   const { start, end } = points;
-    //   points.start = end;
-    //   points.end = start;
-    // }
-
-    // const curve = (t: number) => {
-    //   const { start, end, ctrl } = points;
-    //   return {
-    //     x: (1 - t) * (1 - t) * start.x + 2 * (1 - t) * t * ctrl.x + t * t * end.x,
-    //     y: (1 - t) * (1 - t) * start.y + 2 * (1 - t) * t * ctrl.y + t * t * end.y,
-    //   };
-    // };
-
-    // return [0.3, 0.6, 1]
-    //   .map(v => (frame + v) % 1)
-    //   .map(curve);
-
-    const cmp: any = this.$refs.liquidpath;
-    if (!cmp) {
-      return [];
-    }
-
-    const frameMult = this.highDirection === RIGHT ? this.frame : (1 - this.frame);
-    const totalDistance = cmp.getTotalLength();
-    const frameDistance = frameMult * totalDistance;
-    return [-0.5, 0, 0.5]
-      .map(offsetMult => offsetMult * totalDistance)
-      .map(offset => frameDistance + offset)
-      .filter(distance => distance <= totalDistance)
-      .map(distance => cmp.getPointAtLength(distance));
-  }
-
-  get highArrow() {
-    const x = this.frame * SQUARE_SIZE;
-    const rotateStart = 0.11;
-
-    return {
-      x: this.highDirection === RIGHT ? (75 - x) - 8 : x,
-      y: 23,
-    };
+  get highReversed() {
+    return this.flowOnAngle(RIGHT) > 0;
   }
 
   get lowLiquid(): boolean {
@@ -115,29 +57,16 @@ export default class BridgeTube extends PartComponent {
     <g class="liquid" v-if="lowLiquid" stroke="#4aa0ef">
       <line x1="25" y2="50" x2="25"/>
     </g>
-    <g v-if="lowFlowing" class="outline">
-      <FlowArrow :rotate="lowDirection" :x="lowArrow.x" :y="lowArrow.y"/>
-      <FlowArrow :rotate="lowDirection" :x="lowArrow.x" :y="lowArrow.y - 50"/>
-      <FlowArrow :rotate="lowDirection" :x="lowArrow.x" :y="lowArrow.y + 25"/>
-      <FlowArrow :rotate="lowDirection" :x="lowArrow.x" :y="lowArrow.y - 25"/>
-    </g>
+    <AnimatedArrows v-if="lowFlowing" :reversed="lowReversed" path="M 21,50 V 0"/>
     <!-- high -->
-    <g class="liquid" v-show="highLiquid" stroke="#4aa0ef">
-      <path d="M 50, 25 q -25 -20, -50 0" ref="liquidpath"/>
-    </g>
     <g class="outline">
       <path d="M 50, 21 q -25 -20, -50 0"/>
       <path d="M 50, 29 q -25 -20, -50 0"/>
     </g>
-    <g v-if="highFlowing" class="outline">
-      <FlowArrow
-        v-for="(arrow, idx) in highArrows(frame)"
-        :key="idx"
-        :rotate="highDirection"
-        :x="arrow.x"
-        :y="arrow.y"
-      />
+    <g class="liquid" v-show="highLiquid" stroke="#4aa0ef">
+      <path d="M 50, 25 q -25 -20, -50 0" ref="liquidpath"/>
     </g>
+    <AnimatedArrows v-if="highFlowing" :reversed="highReversed" path="M 50, 29 q -25 -20, -50 0"/>
   </SVGRoot>
 </template>
 
