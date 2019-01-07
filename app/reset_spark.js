897ad273a01845eeac1d1c4b7adf4d23bda4d16f
Bob Steers <steers.bob@gmail.com>
2019-01-07 12:44:33 +0100

    retry reset spark on error status

---
+++ app/reset_spark.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const request = require('request-promise-native');
+require('dotenv').config({path: '.env.development'})
+
+const fileName = process.argv[2];
+const service = process.argv[3];
+const host = process.env.VUE_APP_API_URI;
+
+const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const resetObjects = async () => {
+  const content = JSON.parse(fs.readFileSync(fileName, 'utf8'));
+  for (i = 0; i < 10; i++) {
+    try {
+      const resp = await request.post({
+        uri: `${host}/${service}/reset_objects`,
+        json: content,
+      });
+      console.log(resp);
+      return;
+    } catch (e) {
+      console.log('Retrying after error,', e.message);
+      await sleep(1000);
+    }
+  }
+  throw new Error('Retry attempts exhausted');
+};
+
+resetObjects()
+    .then(() => {
+      console.log(`Succesfully reset objects for ${service}`);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
