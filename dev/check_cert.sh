56477abb0dc4be28165a5a8a8b1efe834419a03b
Bob Steers <steers.bob@gmail.com>
2019-01-15 12:01:58 +0100

    Rename /app directory to /dev for clarity

---
+++ dev/check_cert.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+if [ ! -f "dev/traefik/brewblox.crt" ]; then
+  echo "Certificate not found. Creating a new one...";
+  sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout dev/traefik/brewblox.key -out dev/traefik/brewblox.crt
+  sudo chmod 644 dev/traefik/brewblox.crt
+  sudo chmod 600 dev/traefik/brewblox.key
+fi
