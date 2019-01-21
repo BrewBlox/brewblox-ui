343e1724eb54d7d0d1440bab04028849a53c181a
Elco Jacobs <elco@brewpi.com>
2019-01-21 10:20:04 +0100

    revert broken implementation of rounding in pid output values

--- src/plugins/spark/features/Pid/PidWidget.vue
+++ src/plugins/spark/features/Pid/PidWidget.vue
@@ -72,10 +72,10 @@ export default class PidWidget extends BlockWidget {
         </q-field>
         <q-item>Output</q-item>
         <q-field label="Target">
-          <big>{{ block.data.outputSetting !== null ? (block.data.outputSetting | round) : "--.--" }}</big>
+          <big>{{ block.data.outputSetting | round }}</big>
         </q-field>
         <q-field label="Actual">
-          <big>{{ block.data.outputSetting !== null ? (block.data.outputSetting | round) : "--.--" }}</big>
+          <big>{{ block.data.outputSetting | round }}</big>
         </q-field>
       </div>
       <div :style="gridStyle(8)" class="full-width">
