b9e84d5aa5d41a387b384faf5be7ac8d6aa94520
Bob Steers <steers.bob@gmail.com>
2019-02-25 18:24:37 +0100

    avoid synchronizing outdated local data into the remote store

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -17,7 +17,6 @@ export interface Module {
 const HOST = process.env.VUE_APP_API_URI || window.location.origin;
 const MODULES: Module[] = [];
 const DB_NAME = 'brewblox-ui-store';
-const DB: PouchDB.Database = new PouchDB(DB_NAME);
 
 const cleanId = (moduleId: string, fullId: string): string =>
   fullId.substring(`${moduleId}__`.length);
@@ -43,6 +42,62 @@ const asNewDocument = (moduleId: string, obj: any): any => {
   return asDocument(moduleId, obj);
 };
 
+const databaseInfo =
+  async (db: PouchDB.Database): Promise<PouchDB.Core.DatabaseInfo | null> => {
+    try {
+      return await db.info();
+    } catch (e) {
+      return null;
+    }
+  };
+
+/*
+  We want to synchronize between a client-side local database, and the remote datastore,
+  but avoid contaminating the remote store if it is changed externally.
+
+  On startup, we try to connect to the remote datastore.
+  If it is reachable, we destroy the local store, and enable synchronization between local and remote.
+
+  If the remote datastore is unreachable, we keep the local state, and do not enable synchronization.
+  This scenario is only intended as a fallback: it allows users limited functionality if the datastore service is dead.
+*/
+const promisedDB: Promise<PouchDB.Database> = new Promise((resolve) => {
+  const remoteAddress = `${HOST}/datastore/${DB_NAME}`;
+  const localDb: PouchDB.Database = new PouchDB(DB_NAME);
+  const remoteDb: PouchDB.Database = new PouchDB(remoteAddress);
+
+  databaseInfo(remoteDb)
+    .then(async (remoteInfo) => {
+      if (remoteInfo) {
+        await localDb.destroy();
+      }
+
+      const actualDb = new PouchDB(DB_NAME);
+
+      actualDb
+        /* eslint-disable-next-line @typescript-eslint/camelcase */
+        .changes({ live: true, include_docs: true, since: 'now' })
+        .on('change', (evt: ChangeEvent) => {
+          const handler = MODULES.find(m => checkInModule(m.id, evt.id));
+          if (!handler) {
+            return;
+          }
+          if (evt.deleted) {
+            handler.onDeleted(cleanId(handler.id, evt.id));
+          } else {
+            handler.onChanged(asStoreObject(handler.id, evt.doc));
+          }
+        });
+
+      if (remoteInfo) {
+        actualDb.sync(remoteAddress, { live: true, retry: true });
+      }
+
+      resolve(actualDb);
+    });
+});
+
+
 export function registerModule(module: Module): void {
   if (MODULES.find(m => m.id === module.id)) {
     throw new Error(`Database module "${module.id}" is already registered`);
@@ -51,8 +106,9 @@ export function registerModule(module: Module): void {
 }
 
 export async function fetchAll(moduleId: string): Promise<any[]> {
+  const db = await promisedDB;
   /* eslint-disable-next-line @typescript-eslint/camelcase */
-  const resp = await DB.allDocs({ include_docs: true });
+  const resp = await db.allDocs({ include_docs: true });
   return resp.rows
     .filter(row => checkInModule(moduleId, row.id))
     .map(row => asStoreObject(moduleId, row.doc));
@@ -61,39 +117,25 @@ export async function fetchAll(moduleId: string): Promise<any[]> {
 export async function fetchById<T extends StoreObject>(
   moduleId: string, objId: string
 ): Promise<T> {
-  return asStoreObject(moduleId, await DB.get(fullId(moduleId, objId)));
+  const db = await promisedDB;
+  return asStoreObject(moduleId, await db.get(fullId(moduleId, objId)));
 }
 
 export async function create<T extends StoreObject>(moduleId: string, obj: T): Promise<T> {
-  const resp = await DB.put(asNewDocument(moduleId, obj));
+  const db = await promisedDB;
+  const resp = await db.put(asNewDocument(moduleId, obj));
   return { ...obj, _rev: resp.rev };
 }
 
 export async function persist<T extends StoreObject>(moduleId: string, obj: T): Promise<T> {
-  const resp = await DB.put(asDocument(moduleId, obj));
+  const db = await promisedDB;
+  const resp = await db.put(asDocument(moduleId, obj));
   return { ...obj, _rev: resp.rev };
 }
 
 export async function remove<T extends StoreObject>(moduleId: string, obj: T): Promise<T> {
-  await DB.remove(asDocument(moduleId, obj));
+  const db = await promisedDB;
+  await db.remove(asDocument(moduleId, obj));
   delete obj._rev;
   return obj;
 }
-
-DB
-  /* eslint-disable-next-line @typescript-eslint/camelcase */
-  .changes({ live: true, include_docs: true, since: 'now' })
-  .on('change', (evt: ChangeEvent) => {
-    const handler = MODULES.find(m => checkInModule(m.id, evt.id));
-    if (!handler) {
-      return;
-    }
-    if (evt.deleted) {
-      handler.onDeleted(cleanId(handler.id, evt.id));
-    } else {
-      handler.onChanged(asStoreObject(handler.id, evt.doc));
-    }
-  });
-
-DB
-  .sync(`${HOST}/datastore/${DB_NAME}`, { live: true, retry: true });
