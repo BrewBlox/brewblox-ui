eb5d690a4a01d66a9bc4007e2ac96d90ef8c5e6f
Bob Steers <steers.bob@gmail.com>
2019-04-08 16:34:52 +0200

    remove unused import

--- src/plugins/spark/features/ProcessView/components/PartCard.ts
+++ src/plugins/spark/features/ProcessView/components/PartCard.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { FlowPart, PersistentPart } from '../state';
+import { FlowPart } from '../state';
 
 @Component({
   props: {
