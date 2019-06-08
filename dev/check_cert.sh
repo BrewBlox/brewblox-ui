761aa3494b0d48f1a3c52f7188d7ef86118391c1
Bob Steers <steers.bob@gmail.com>
2019-06-08 19:57:02 +0200

    create dev/traefik in cert check

--- dev/check_cert.sh
+++ dev/check_cert.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+mkdir -p dev/traefik
+
 if [ ! -f "dev/traefik/brewblox.crt" ]; then
   echo "Certificate not found. Creating a new one...";
   sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout dev/traefik/brewblox.key -out dev/traefik/brewblox.crt
