eef6cf82223b2d406f48f7ed58be5cbacbd8d881
Bob Steers <steers.bob@gmail.com>
2019-06-17 15:38:59 +0200

    link motor valve to process view part

---
+++ src/plugins/spark/features/ProcessView/components/ValvePartCard.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+
+import PartCard from './PartCard';
+
+@Component
+export default class ValvePartCard extends PartCard {
+  get blockCardProps() {
+    return {
+      ...this.$props,
+      types: ['MotorValve'],
+      blockServiceIdKey: 'valveServiceId',
+      blockLinkKey: 'valveLink',
+    };
+  }
+}
+</script>
+
+<template>
+  <BlockPartCard v-bind="blockCardProps" v-on="$listeners"/>
+</template>
--- src/plugins/spark/features/ProcessView/specs/ActuatorValve.ts
+++ src/plugins/spark/features/ProcessView/specs/ActuatorValve.ts
@@ -1,12 +1,12 @@
 import sparkStore from '@/plugins/spark/store';
-import { Block } from '@/plugins/spark/types';
+import { Block, DigitalState } from '@/plugins/spark/types';
 
 import { LEFT, RIGHT, defaultSpec } from '../getters';
 import { ComponentSpec, StatePart, Transitions } from '../types';
 
 const spec: ComponentSpec = {
   ...defaultSpec,
-  cards: ['ActuatorPartCard'],
+  cards: ['ValvePartCard'],
   transitions: (part: StatePart): Transitions =>
     ((part.state || {}).closed)
       ? {}
@@ -15,14 +15,16 @@ const spec: ComponentSpec = {
         [RIGHT]: [{ outCoords: LEFT }],
       },
   interactHandler: (part: StatePart) => {
-    const serviceId = part.settings.actuatorServiceId;
-    const link = part.settings.actuatorLink;
+    const serviceId = part.settings.valveServiceId;
+    const link = part.settings.valveLink;
     if (!serviceId || !link || !link.id) {
       return;
     }
     const block: Block = sparkStore.blocks(serviceId)[link.id];
     if (block) {
-      block.data.state = !!part.state.closed ? 1 : 0;
+      block.data.state = !!part.state.closed
+        ? DigitalState.Active
+        : DigitalState.Inactive;
       sparkStore.saveBlock([serviceId, block]);
     }
   },
