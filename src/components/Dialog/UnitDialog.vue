c90f5d5fd65986e2eef2bd2d35f5496c2ad8e92d
Bob Steers <steers.bob@gmail.com>
2019-07-16 16:55:22 +0200

    fix rounding in unit dialog

--- src/components/Dialog/UnitDialog.vue
+++ src/components/Dialog/UnitDialog.vue
@@ -2,6 +2,7 @@
 import { Component, Prop } from 'vue-property-decorator';
 
 import DialogBase from '@/components/Dialog/DialogBase';
+import { roundNumber } from '@/helpers/functional';
 import { Unit } from '@/helpers/units';
 
 @Component
@@ -11,11 +12,16 @@ export default class UnitDialog extends DialogBase {
   @Prop({ type: Object, required: true, validator: v => v instanceof Unit })
   public readonly value!: Unit;
 
+  @Prop({ type: Number, default: 2 })
+  readonly decimals!: number;
+
   @Prop({ type: String, default: 'Value' })
   public readonly label!: string;
 
   created() {
-    this.local = this.value.value;
+    this.local = this.value.value !== null
+      ? roundNumber(this.value.value, this.decimals)
+      : null;
   }
 
   save() {
@@ -29,7 +35,7 @@ export default class UnitDialog extends DialogBase {
     <q-card class="q-dialog-plugin q-dialog-plugin--dark" dark>
       <q-card-section class="q-dialog__title">{{ title }}</q-card-section>
       <q-card-section v-if="message" class="q-dialog__message scroll">{{ message }}</q-card-section>
-      <q-card-section v-if="messageHtml" class="q-dialog__message scroll" v-html="messageHtml"/>
+      <q-card-section v-if="messageHtml" class="q-dialog__message scroll" v-html="messageHtml" />
       <q-card-section class="scroll">
         <q-input
           v-model.number="local"
@@ -45,8 +51,8 @@ export default class UnitDialog extends DialogBase {
         </q-input>
       </q-card-section>
       <q-card-actions align="right">
-        <q-btn flat label="Cancel" color="primary" @click="onDialogCancel"/>
-        <q-btn flat label="OK" color="primary" @click="save"/>
+        <q-btn flat label="Cancel" color="primary" @click="onDialogCancel" />
+        <q-btn flat label="OK" color="primary" @click="save" />
       </q-card-actions>
     </q-card>
   </q-dialog>
