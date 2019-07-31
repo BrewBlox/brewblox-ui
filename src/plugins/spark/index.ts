a6e6c5375c23b39529f0ecf641e630c82c31d978
Bob Steers <steers.bob@gmail.com>
2019-07-31 21:12:19 +0200

    add some tests for actuator offset

--- src/plugins/spark/index.ts
+++ src/plugins/spark/index.ts
@@ -1,17 +1,6 @@
-import Vue from 'vue';
+import { VueConstructor } from 'vue';
 
 import { autoRegister } from '@/helpers/component-ref';
-import {
-  base64ToHex,
-  dateString,
-  durationString,
-  hexToBase64,
-  round,
-  shortDateString,
-  truncate,
-  unitDurationString,
-} from '@/helpers/functional';
-import { Link, Unit } from '@/helpers/units';
 import { sparkStore } from '@/plugins/spark/store';
 import { featureStore } from '@/store/features';
 import { providerStore } from '@/store/providers';
@@ -20,10 +9,8 @@ import { Service } from '@/store/services';
 import arrangements from './arrangements';
 import features from './features';
 import { typeName } from './getters';
+import { installFilters } from './helpers';
 import { BlockSpec } from './types';
-
-autoRegister(require.context('./components', true, /[A-Z]\w+\.vue$/));
-autoRegister(require.context('./provider', true, /[A-Z]\w+\.vue$/));
 
 const onAdd = async (service: Service): Promise<void> => {
   await sparkStore.addService(service.id);
@@ -43,23 +30,11 @@ const onRemove = async (service: Service): Promise<void> => {
 };
 
 export default {
-  install() {
-    Vue.filter(
-      'unit',
-      (value: Unit | null) =>
-        (value !== null && value !== undefined ? value.toString() : '-'));
-    Vue.filter(
-      'link',
-      (value: Link | null) =>
-        (value !== null && value !== undefined ? value.toString() : '-'));
-    Vue.filter('round', round);
-    Vue.filter('hexToBase64', hexToBase64);
-    Vue.filter('base64ToHex', base64ToHex);
-    Vue.filter('duration', durationString);
-    Vue.filter('truncated', truncate);
-    Vue.filter('unitDuration', unitDurationString);
-    Vue.filter('dateString', dateString);
-    Vue.filter('shortDateString', shortDateString);
+  install(Vue: VueConstructor) {
+    installFilters(Vue);
+
+    autoRegister(require.context('./components', true, /[A-Z]\w+\.vue$/));
+    autoRegister(require.context('./provider', true, /[A-Z]\w+\.vue$/));
 
     Object.values(features)
       .forEach(feature => featureStore.createFeature(feature.feature));
