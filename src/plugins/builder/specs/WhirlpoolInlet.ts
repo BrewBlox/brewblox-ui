43f2cfb8908d15d8fb636cf792aa3898a2774c3c
Bob Steers <steers.bob@gmail.com>
2019-09-10 12:41:52 +0200

    move outlet whirlpool to correct position

--- src/plugins/builder/specs/WhirlpoolInlet.ts
+++ src/plugins/builder/specs/WhirlpoolInlet.ts
@@ -1,16 +1,21 @@
+import { Coordinates } from '@/helpers/coordinates';
+
 import { CENTER, LEFT } from '../getters';
 import { PartSpec } from '../types';
 
 const SIZE_X = 1;
 const SIZE_Y = 4;
+const OUT = new Coordinates(CENTER)
+  .translate([0, SIZE_Y, 0])
+  .toString();
 
 const spec: PartSpec = {
   id: 'WhirlpoolInlet',
   cards: [],
   size: () => [SIZE_X, SIZE_Y],
   transitions: () => ({
-    [LEFT]: [{ outCoords: CENTER }],
-    [CENTER]: [{ outCoords: LEFT }],
+    [LEFT]: [{ outCoords: OUT }],
+    [OUT]: [{ outCoords: LEFT }],
   }),
 };
 
