697b706aeb449c4ee66a13648661b5bf7fa3f3d7
Bob Steers <steers.bob@gmail.com>
2019-12-08 14:32:00 +0100

    refactor relation styling

--- src/helpers/import-export.ts
+++ src/helpers/import-export.ts
@@ -1,4 +1,5 @@
 import FileSaver from 'file-saver';
+import html2canvas from 'html2canvas';
 
 import { serialize } from './units/parseObject';
 
@@ -7,4 +8,15 @@ export function saveFile(exported: any, title: string, raw = false): void {
   const vals = raw ? exported : JSON.stringify(serialize(exported));
   const blob = new Blob([vals], { type: 'text/plain;charset=utf-8' });
   FileSaver.saveAs(blob, title);
+}
+
+export function screenshot(el: HTMLElement, filename: string): void {
+  html2canvas(el).then(canvas => {
+    const link = document.createElement('a');
+    link.download = filename;
+    canvas.toBlob(blob => {
+      link.href = URL.createObjectURL(blob);
+      link.click();
+    });
+  });
 }
