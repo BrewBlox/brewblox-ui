957fd8fbbe27b6bdd5afca2ed7e2899b05c65b28
Bob Steers <steers.bob@gmail.com>
2019-12-13 15:26:23 +0100

    fix endless loop when using invalid prefix

--- src/plugins/quickstart/Herms/HermsNamingTask.vue
+++ src/plugins/quickstart/Herms/HermsNamingTask.vue
@@ -12,6 +12,7 @@ import { sparkStore } from '@/plugins/spark/store';
 import { Service, serviceStore } from '@/store/services';
 
 import WizardTaskBase from '../components/WizardTaskBase';
+import { maybeSpace } from '../helpers';
 import { HermsBlockNames, HermsConfig } from './types';
 
 
@@ -92,7 +93,7 @@ export default class HermsNamingTask extends WizardTaskBase<HermsConfig> {
   get names(): HermsBlockNames {
     return {
       ...mapValues(this.defaultNames,
-        v => suggestId(`${this.prefix} ${v}`, validator(blockIdRules(this.serviceId)))),
+        v => suggestId(maybeSpace(this.prefix, v), validator(blockIdRules(this.serviceId)))),
       ...this.chosenNames,
     };
   }
@@ -175,17 +176,10 @@ export default class HermsNamingTask extends WizardTaskBase<HermsConfig> {
             The name for the new dashboard.
           </template>
         </QuickStartNameField>
-        <QuickStartNameField
+        <QuickStartPrefixField
           v-model="prefix"
-          optional
-          label="Prefix for names"
           @clear="clearKey('prefix')"
-        >
-          <template #help>
-            By default all block names are prefixed.
-            You can override this for individual blocks.
-          </template>
-        </QuickStartNameField>
+        />
 
         <!-- Block names -->
         <q-expansion-item label="Generated names" icon="mdi-tag-multiple" dense>
