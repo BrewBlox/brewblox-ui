a2b25d9408893e4626e8037690b4eecf116cd747
Bob Steers <steers.bob@gmail.com>
2019-12-18 22:54:54 +0100

    dependency cleanup

--- src/helpers/import-export.ts
+++ src/helpers/import-export.ts
@@ -1,9 +1,37 @@
-import FileSaver from 'file-saver';
+import get from 'lodash/get';
+import * as quasar from 'quasar';
 
-import { serialize } from './units/parseObject';
+import { deserialize, serialize } from './units/parseObject';
+
+// exportFile is missing from the typings, even though it is implemented
+// TODO(Bob): replace with correct import once it's fixed in quasar
+const exportFile: ((title: string, content: string) => void) = quasar['exportFile'];
 
 export function saveFile(exported: any, title: string, raw = false): void {
-  const vals = raw ? exported : JSON.stringify(serialize(exported));
-  const blob = new Blob([vals], { type: 'text/plain;charset=utf-8' });
-  FileSaver.saveAs(blob, title);
+  const content = raw ? exported : JSON.stringify(serialize(exported));
+  exportFile(title, content);
 }
+
+export function loadFile<T>(onSelected: (val: T) => void): void {
+  const reader: FileReader = new FileReader();
+  const input: HTMLInputElement = document.createElement('input');
+
+  input.setAttribute('type', 'file');
+  input.setAttribute('id', 'import-input');
+
+  reader.onload = evt => {
+    const str = get(evt, ['target', 'result'], '');
+    if (str) {
+      onSelected(deserialize(JSON.parse(str)));
+    }
+  };
+
+  input.onchange = evt => {
+    const file = get(evt, ['target', 'files', 0]);
+    if (file) {
+      reader.readAsText(file);
+    }
+  };
+
+  input.click();
+};
