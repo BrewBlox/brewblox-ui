a593941b8c355799c4b3bd50bc3047046e8edf08
Bob Steers <steers.bob@gmail.com>
2020-05-25 16:22:42 +0200

    Implement scaling for non-tube parts; toggle flowable

--- src/plugins/builder/parts/PwmDisplay.vue
+++ src/plugins/builder/parts/PwmDisplay.vue
@@ -2,12 +2,18 @@
 import { Component } from 'vue-property-decorator';
 
 import PartBase from '../components/PartBase';
+import { CENTER } from '../getters';
 
 
 @Component
-export default class PwmDisplay extends PartBase { }
+export default class PwmDisplay extends PartBase {
+
+  get color(): string {
+    return this.liquidOnCoord(CENTER)[0] ?? '';
+  }
+}
 </script>
 
 <template>
-  <PwmValues :part="part" settings-key="pwm" />
+  <PwmValues :part="part" settings-key="pwm" :color="color" />
 </template>
--- src/plugins/builder/specs/SetpointDisplay.ts
+++ src/plugins/builder/specs/SetpointDisplay.ts
@@ -1,48 +1,50 @@
-import { Coordinates } from '@/helpers/coordinates';
 import { blockTypes } from '@/plugins/spark/block-types';
 
-import { CENTER, DOWN, LEFT, RIGHT, UP } from '../getters';
-import { showSettingsBlock } from '../helpers';
+import { showSettingsBlock, universalTransitions } from '../helpers';
 import { PartSpec } from '../types';
 
-
-const RIGHT_RIGHT = new Coordinates(RIGHT).translate([1, 0, 0]).toString();
-const RIGHT_UP = new Coordinates(UP).translate([1, 0, 0]).toString();
-const RIGHT_DOWN = new Coordinates(DOWN).translate([1, 0, 0]).toString();
 
 const SIZE_X = 2;
 const SIZE_Y = 1;
 const settingsKey = 'setpoint';
+const scaleKey = 'scale';
+const flowEnabledKey = 'flowEnabled';
+
+const size: PartSpec['size'] = ({ settings }) => {
+  const scale = settings[scaleKey] ?? 1;
+  return [SIZE_X * scale, SIZE_Y * scale];
+};
 
 const spec: PartSpec = {
   id: 'SetpointDisplay',
   title: 'Display: setpoint',
-  cards: [{
-    component: 'BlockAddressCard',
-    props: {
-      settingsKey,
-      compatible: [blockTypes.SetpointSensorPair],
-      label: 'Setpoint',
+  cards: [
+    {
+      component: 'BlockAddressCard',
+      props: {
+        settingsKey,
+        compatible: [blockTypes.SetpointSensorPair],
+        label: 'Setpoint',
+      },
     },
-  }],
-  size: () => [SIZE_X, SIZE_Y],
+    {
+      component: 'ScaleCard',
+      props: {
+        settingsKey: scaleKey,
+        defaultSize: [SIZE_X, SIZE_Y],
+      },
+    },
+    {
+      component: 'ToggleCard',
+      props: {
+        settingsKey: flowEnabledKey,
+        label: 'Allow liquid to flow through this part',
+      },
+    },
+  ],
+  size,
+  transitions: part => universalTransitions(size(part), part.settings[flowEnabledKey]),
   interactHandler: part => showSettingsBlock(part, settingsKey),
-  transitions: () => ({
-    [UP]: [{ outCoords: CENTER, internal: true, friction: 0.5 }],
-    [LEFT]: [{ outCoords: CENTER, internal: true, friction: 0.5 }],
-    [DOWN]: [{ outCoords: CENTER, internal: true, friction: 0.5 }],
-    [RIGHT_RIGHT]: [{ outCoords: CENTER, internal: true, friction: 0.5 }],
-    [RIGHT_UP]: [{ outCoords: CENTER, internal: true, friction: 0.5 }],
-    [RIGHT_DOWN]: [{ outCoords: CENTER, internal: true, friction: 0.5 }],
-    [CENTER]: [
-      { outCoords: UP, friction: 0.5 },
-      { outCoords: LEFT, friction: 0.5 },
-      { outCoords: DOWN, friction: 0.5 },
-      { outCoords: RIGHT_RIGHT, friction: 0.5 },
-      { outCoords: RIGHT_UP, friction: 0.5 },
-      { outCoords: RIGHT_DOWN, friction: 0.5 },
-    ],
-  }),
 };
 
 export default spec;
