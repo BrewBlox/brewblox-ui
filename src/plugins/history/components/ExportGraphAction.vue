3dbd5bccc566d2015c233822dc3cea76c71ab152
Bob Steers <steers.bob@gmail.com>
2020-06-12 14:13:54 +0200

    Choose CSV output timestamp

--- src/plugins/history/components/ExportGraphAction.vue
+++ src/plugins/history/components/ExportGraphAction.vue
@@ -2,9 +2,21 @@
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 
+import { createDialog } from '@/helpers/dialog';
 import { saveFile } from '@/helpers/import-export';
+import notify from '@/helpers/notify';
 import { historyStore } from '@/plugins/history/store';
 import { GraphConfig, QueryParams, QueryTarget } from '@/plugins/history/types';
+
+const precisionOpts: SelectOption[] = [
+  { label: 'Nanoseconds', value: 'ns' },
+  { label: 'Microseconds', value: 'µ' },
+  { label: 'Milliseconds', value: 'ms' },
+  { label: 'Seconds', value: 's' },
+  { label: 'Minutes', value: 'm' },
+  { label: 'Hours', value: 'h' },
+  { label: '[YYYY]-[MM]-[DD]T[hh]:[mm]:[ss]Z', value: 'ISO8601' },
+];
 
 @Component
 export default class ExportGraphAction extends Vue {
@@ -21,18 +33,41 @@ export default class ExportGraphAction extends Vue {
   @Prop({ type: Object, required: true })
   public readonly config!: GraphConfig;
 
-  async fetchTarget(params: QueryParams, target: QueryTarget): Promise<void> {
-    const result = await historyStore.fetchValues([params, target]);
+  async fetchTarget(params: QueryParams, target: QueryTarget, timestamp: string): Promise<void> {
+    const isText = timestamp === 'ISO8601';
+    const epoch = isText ? 'ms' : timestamp;
+    notify.info('Generating CSV file... This may take a few seconds.');
+    const result = await historyStore.fetchValues([params, target, epoch]);
+    if (isText) {
+      result
+        .values
+        .forEach(slice => slice[0] = new Date(slice[0]).toISOString() as any);
+    }
     const lines: string[] = [
       result.columns.join(),
-      ...result.values.map(slice => slice.map(v => v === null ? '' : v.toString()).join()),
+      ...result
+        .values
+        .map(slice => slice.map(v => v === null ? '' : v.toString()).join()),
     ];
     saveFile(lines.join('\n'), `graph-${this.header}-${result.name}-${result.policy}.csv`, true);
   }
 
   async exportData(): Promise<void> {
-    const params: QueryParams = { ...this.config.params, policy: 'downsample_1m' };
-    await Promise.all(this.config.targets.map(target => this.fetchTarget(params, target)));
+    createDialog({
+      component: 'SelectDialog',
+      value: precisionOpts.find(v => v.value === 'ms'),
+      selectOptions: precisionOpts,
+      title: 'Select timestamp formatting',
+      message: 'Numeric values will show time elapsed since epoch (1 jan 1970).',
+      selectProps: {
+        label: 'Formatting',
+      },
+    })
+      .onOk(async (opt: SelectOption) => {
+        if (!opt) { return; }
+        const params: QueryParams = { ...this.config.params, policy: 'downsample_1m' };
+        await Promise.all(this.config.targets.map(target => this.fetchTarget(params, target, opt.value)));
+      });
   }
 }
 </script>
