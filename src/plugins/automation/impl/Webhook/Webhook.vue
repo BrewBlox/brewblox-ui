b67b4b19130eb50885c61d42e261268cb2797c8b
Bob Steers <steers.bob@gmail.com>
2020-06-22 14:01:33 +0200

    make webhook request headers editable

--- src/plugins/automation/impl/Webhook/Webhook.vue
+++ src/plugins/automation/impl/Webhook/Webhook.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+import toPairs from 'lodash/toPairs';
 import { Component } from 'vue-property-decorator';
 
 import { createDialog } from '@/helpers/dialog';
@@ -7,7 +8,13 @@ import AutomationItemBase from '@/plugins/automation/components/AutomationItemBa
 import { previewWebhook } from '@/plugins/automation/store/preview-api';
 import { WebhookImpl } from '@/plugins/automation/types';
 
-@Component
+import HeadersDialog from './HeadersDialog.vue';
+
+@Component({
+  components: {
+    HeadersDialog,
+  },
+})
 export default class Webhook extends AutomationItemBase<WebhookImpl> {
   busy = false;
 
@@ -15,10 +22,15 @@ export default class Webhook extends AutomationItemBase<WebhookImpl> {
     return { label: this.impl.method, value: this.impl.method };
   }
 
+  get prettyHeaders(): string {
+    return toPairs(this.impl.headers)
+      .map(items => items.join(': '))
+      .join('\n');
+  }
+
   editUrl(): void {
     createDialog({
       component: 'InputDialog',
-      parent: this,
       label: 'URL',
       title: 'Edit request URL',
       value: this.impl.url,
@@ -29,12 +41,25 @@ export default class Webhook extends AutomationItemBase<WebhookImpl> {
       });
   }
 
+  editHeaders(): void {
+    createDialog({
+      component: HeadersDialog,
+      title: 'Edit request headers',
+      value: this.impl.headers,
+    })
+      .onOk(headers => {
+        this.impl.headers = headers;
+        this.save();
+      });
+  }
+
   editBody(): void {
     createDialog({
       component: 'TextAreaDialog',
       parent: this,
       label: 'Body',
       title: 'Edit request body',
+      autogrow: false,
       value: this.impl.body,
     })
       .onOk(body => {
@@ -71,6 +96,19 @@ export default class Webhook extends AutomationItemBase<WebhookImpl> {
       class="col-grow"
       @input="v => {impl.method = v; save()}"
     />
+
+    <div class="col-break" />
+
+    <LabeledField
+      title="Message headers"
+      label="Message headers"
+      :readonly="false"
+      class="col-grow"
+      tag-style="white-space: pre-line; margin-top: -0.5em"
+      @click="editHeaders"
+    >
+      {{ prettyHeaders || 'Click to edit' }}
+    </LabeledField>
 
     <div class="col-break" />
 
