9dedcde1fb852f6d10f9e7c21223c3a81359dcd4
Bob Steers <steers.bob@gmail.com>
2020-10-27 22:55:02 +0100

    better guesswork for default port

--- src/helpers/const.ts
+++ src/helpers/const.ts
@@ -1,16 +1,16 @@
 const devHostname = process.env.BLOX_API_HOST;
 const devPort = process.env.BLOX_API_PORT;
 
+export const PROTOCOL = window.location.protocol.replace(':', '');
+export const WS_PROTOCOL = PROTOCOL.replace('http', 'ws') as 'ws' | 'wss';
+
 export const HOSTNAME = process.env.DEV
   ? devHostname
   : window.location.hostname;
 
 export const PORT = process.env.DEV
   ? Number(devPort)
-  : Number(window.location.port) || 443;
-
-export const PROTOCOL = window.location.protocol.replace(':', '');
-export const WS_PROTOCOL = PROTOCOL.replace('http', 'ws') as 'ws' | 'wss';
+  : Number(window.location.port) || PROTOCOL === 'https' ? 443 : 80;
 
 export const HOST = `${PROTOCOL}://${HOSTNAME}:${PORT}`;
 export const WS_HOST = `${WS_PROTOCOL}://${HOSTNAME}:${PORT}`;
