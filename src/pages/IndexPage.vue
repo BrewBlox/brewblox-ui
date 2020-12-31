a863e111224bd453b3fd7a613825d8bfffa02a7d
Bob Steers <steers.bob@gmail.com>
2020-12-31 13:50:09 +0100

    Improve homepage selection in indexpage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,23 +4,41 @@ import { Component } from 'vue-property-decorator';
 import { Watch } from 'vue-property-decorator';
 
 import { objectSorter } from '@/helpers/functional';
+import { builderStore } from '@/plugins/builder/store';
 import { dashboardStore } from '@/store/dashboards';
+import { serviceStore } from '@/store/services';
 import { systemStore } from '@/store/system';
 
 @Component
 export default class IndexPage extends Vue {
 
   get homePage(): string | null {
-    return systemStore.config.homePage
-      ?? [...dashboardStore.dashboards]
-        .sort(objectSorter('order'))
-        .map(v => `/dashboard/${v.id}`)[0]
-      ?? null;
+    const { homePage } = systemStore.config;
+
+    const defaultPage = [...dashboardStore.dashboards]
+      .sort(objectSorter('order'))
+      .map(v => `/dashboard/${v.id}`)[0] ?? null;
+
+    // Defaults to first dashboard
+    if (!homePage) {
+      return defaultPage;
+    }
+
+    const [, section, pageId] = homePage.split('/');
+    const valid = (
+      (section === 'dashboard' && dashboardStore.dashboardIds.includes(pageId)) ||
+      (section === 'service' && serviceStore.serviceIds.includes(pageId)) ||
+      (section === 'brewery' && builderStore.layoutIds.includes(pageId))
+    );
+
+    return valid
+      ? homePage
+      : defaultPage;
   }
 
   @Watch('homePage', { immediate: true })
-  onPrimaryDashboardFound(): void {
-    if (this.homePage !== null) {
+  onHomePageFound(): void {
+    if (this.homePage) {
       this.$router.replace(this.homePage);
     }
   }
