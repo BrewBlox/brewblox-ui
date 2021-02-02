e7e681d78f8aaf16f1925903a460cc9eb26667c6
Bob Steers <steers.bob@gmail.com>
2021-02-02 16:02:55 +0100

    add help text in mode dialog

--- src/plugins/quickstart/TempControl/TempControlModeDialog.vue
+++ src/plugins/quickstart/TempControl/TempControlModeDialog.vue
@@ -214,6 +214,58 @@ export default class TempControlModeDialog extends DialogBase {
         />
       </q-card-section>
 
+      <q-card-section class="text-weight-light q-pt-none q-mx-xs">
+        <q-item-label class="text-subtitle1">
+          Temperature control with PID
+        </q-item-label>
+        <p>
+          The minimal building blocks for a control system are:
+          <ul>
+            <li>A sensor, to measure what you want to control.</li>
+            <li>A setpoint, the target value for the sensor.</li>
+            <li>An actuator, to drive the sensor value towards the setpoint.</li>
+            <li>
+              A controller, in our case a PID,
+              to calculate what the value for the actuator should be
+              from the sensor and setpoint value.
+            </li>
+          </ul>
+        </p>
+
+        <p>
+          Because the system can respond differently
+          to the heater and the cooler, each has its own PID. <br>
+          For more information, visit our <a
+            href="https://brewblox.netlify.app/user/control_chains.html"
+            target="_blank"
+            style="color: white"
+          >control chains guide</a>.
+        </p>
+
+        <q-item-label class="text-subtitle1">
+          Control modes
+        </q-item-label>
+        <p>
+          Control modes let you toggle between PID configurations.
+        </p>
+        <p>
+          When you control fridge air temperature,
+          the measured temperature will change more quickly
+          than when you control a large volume of beer. <br>
+          This requires different PID settings.
+          By using control modes, you can store the settings, and switch between them.
+        </p>
+        <p>
+          PID settings can still be tuned by changing the block settings.
+          The Temperature Control widget detects changes,
+          and will offer to either update or re-apply the mode settings.
+        </p>
+        <p>
+          Each mode uses its own Setpoint and Temperature Sensor.
+          When applying a mode, the PID input Setpoint is changed.
+        </p>
+      </q-card-section>
+
       <template v-if="showConfirm" #actions>
         <q-btn
           unelevated
