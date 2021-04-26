77e9c22c27c234e18c0498c8ed971f09135d8ef7
Bob Steers <steers.bob@gmail.com>
2021-04-26 18:47:40 +0200

    remove unused dependency vue-json-pretty

--- src/boot/externals.ts
+++ src/boot/externals.ts
@@ -1,8 +1,6 @@
 import { boot } from 'quasar/wrappers';
-import VueJsonPretty from 'vue-json-pretty';
 import draggable from 'vuedraggable';
 
 export default boot(({ app }) => {
   app.component('Draggable', draggable);
-  app.component('VueJsonPretty', VueJsonPretty);
 });
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,4 +1,3 @@
-declare module 'vue-json-pretty';
 declare module 'url-safe-string';
 declare module 'parse-duration';
 declare module 'fromentries';
