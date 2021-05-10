096aa263903b7cd7b115d5d1173c80ab9cb84b8d
Bob Steers <steers.bob@gmail.com>
2021-05-10 12:55:09 +0200

    only replace compression plugin if it exists

--- quasar.conf.js
+++ quasar.conf.js
@@ -127,11 +127,10 @@ module.exports = configure(function (ctx) {
         };
 
         // Replace the compression plugin because it was generating unnamed output files
-        config.plugins.splice(
-          config.plugins.findIndex(v => v instanceof CompressionPlugin),
-          1,
-          new CompressionPlugin(),
-        );
+        const compressionIdx = config.plugins.findIndex(v => v instanceof CompressionPlugin);
+        if (compressionIdx >= 0) {
+          config.plugins.splice(compressionIdx, 1, new CompressionPlugin());
+        }
 
         config.plugins.push(
           // mqtt.js depends on multiple Node.JS libraries
