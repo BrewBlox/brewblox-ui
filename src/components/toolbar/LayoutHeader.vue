971730172b24c916550a04c109bd4088bfc51cf4
Bob Steers <steers.bob@gmail.com>
2021-05-13 12:29:31 +0200

    show title centered in wide layouts

--- src/components/toolbar/LayoutHeader.vue
+++ src/components/toolbar/LayoutHeader.vue
@@ -22,13 +22,21 @@ export default defineComponent({
   <q-header class="bg-dark shadow-1">
     <q-bar class="bg-transparent q-px-none">
       <q-btn flat round icon="menu" style="font-size: 80%" @click="$emit('menu')" />
-      <div v-if="!dense" class="col-auto" style="font-weight: 500; font-size: 110%">
-        <slot name="title" />
-      </div>
-      <q-btn flat rounded to="/" class="absolute-center">
-        <img v-if="dense" src="~assets/logo-x.svg" style="height: 30px" class="q-py-xs">
-        <img v-else src="~assets/logo-wordmark-dark.svg" style="height: 30px" class="q-py-xs">
-      </q-btn>
+      <!-- Dense: Home button centered, no title -->
+      <template v-if="dense">
+        <q-btn flat rounded to="/" class="absolute-center">
+          <img src="~assets/logo-x.svg" style="height: 30px" class="q-py-xs">
+        </q-btn>
+      </template>
+      <!-- Wide: Home button left, title centered -->
+      <template v-else>
+        <q-btn flat rounded to="/">
+          <img src="~assets/logo-wordmark-dark.svg" style="height: 30px" class="q-py-xs">
+        </q-btn>
+        <div class="col-auto absolute-center" style="font-weight: 500; font-size: 110%">
+          <slot name="title" />
+        </div>
+      </template>
       <q-space />
       <slot name="buttons" />
     </q-bar>
